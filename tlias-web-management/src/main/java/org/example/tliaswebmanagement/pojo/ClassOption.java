274788ee3bc187a940e6fa88cfccaebb2dd242d6
ize250917 <GitHub>
2026-07-07 00:24:13 +0800

    新增了学员管理中的分页查询

---
+++ tlias-web-management/src/main/java/org/example/tliaswebmanagement/pojo/ClassOption.java
@@ -0,0 +1,15 @@
+package org.example.tliaswebmanagement.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassOption {
+    private List classList; //班级名称
+    private List dataList; //班级人数
+}
